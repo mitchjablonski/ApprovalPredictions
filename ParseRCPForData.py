2380dadf09a32dafb58f3582ec4510620ec7defd
bigdogstudio <mitchjabdota@gmail.com>
2018-04-09 21:28:42 -0400

    Cleans up all of the dates for the polling data
    Returns a start and end date in the format that google wants for the CSE

--- ParseRCPForData.py
+++ ParseRCPForData.py
@@ -10,40 +10,10 @@ from bs4 import BeautifulSoup
 import pandas as pd
 from collections import defaultdict
 
-
-content = urllib2.urlopen("https://www.realclearpolitics.com/epolls/other/president_trump_job_approval-6179.html")
-soup = BeautifulSoup(content,'html.parser')
-#print(soup)
-tables = soup.findAll('table', {'class': 'data'})
-
-##1st table in page index 0 is only recent polls, 2nd table index 1 is all data.
-table = tables[1]
-
-rows = [row for row in table.find_all("tr")]
-columns = [str(col.get_text()) for col in rows[0].find_all("th")]
-pollingHeaders = [column.split('(')[0].strip() for column in columns[:]]
-
-pollingData = []
-
-for row in rows:
-    tds = row.find_all("td")    
-    pollingData.append([(str(t.get_text())) for t in tds[:]])
-
-pollingDF = pd.DataFrame(data = pollingData[1:], columns = pollingHeaders)
-
-#pollingDict = defaultdict(list)
-#for currData in pollingData:
-#    if currData != []:
-#        for currNum, currHeader in enumerate(pollingHeaders):
-#            pollingDict[currHeader].append(currData[currNum])
-#pollingDF = pd.DataFrame(data=None, columns=pollingHeaders)
-#
-#for headers in pollingHeaders:
-#    pollingDF[headers] = pollingDict[headers]
 def ConvertToTwoDigitDate(currDate):
     newDate = currDate
     if (int(currDate) < 10):
-        '0' + currDate
+        newDate = '0' + currDate
     return newDate
 
 def ConvertToDesiredFormat(year, month, day):
@@ -56,47 +26,71 @@ def GenerateGoogleFormatDate(year, monthAndDay):
     newDay   = ConvertToTwoDigitDate(monthAndDay[1])
     return ConvertToDesiredFormat(year, newMonth, newDay)
     
-def ModifyDatesForYear(dateData):
-    dateDF = pd.DataFrame(data=None, columns = ['StartDate', 'EndDate'])
+def ModifyDatesForYear(dateData, latestYear):
     pollingDates = defaultdict(list)
-    currYear = '2018'
+    currYear = latestYear
     startDateLastYear = False
     endDateLastYear = False
+    yearModified    = False
     ##Desired format YYYY-MM-DD
     for dates in dateData:
         dateList = dates.split()
         dateList = [dateList[0], dateList[2]]
         #item 1 in our dateList is our start poll, 2 is our end poll
         for currNum, monthDay in enumerate(dateList):
+            monthDay = monthDay.split('/')
+            tempYear = currYear
             ##item Last Year
-            if currNum == 1 & int(monthDay[0]) == 12:
+            if int(monthDay[0]) == 12 and yearModified == False:
                 tempYear = str(int(currYear) -1)
-                monthDay = monthDay.split('/')
-                newDate = GenerateGoogleFormatDate(tempYear, monthDay)
-                #newMonth = ConvertToTwoDigitDate(currItem[0])
-                #newDay   = ConvertToTwoDigitDate(currItem[1])
-                #newDate = ConvertToDesiredFormat(tempYear, newMonth, newDay)
+                if currNum == 0:
+                    startDateLastYear = True
+                else:
+                    endDateLastYear = True
+                    
+            newDate = GenerateGoogleFormatDate(tempYear, monthDay)
+            if currNum == 0:
                 pollingDates['StartDate'].append(newDate)
-                startDateLastYear = True
-            elif currNum == 2 & int(monthDay[0]) == 12:
-                tempYear = str(int(currYear) -1)
-                monthDay = monthDay.split('/')
-                newDate = GenerateGoogleFormatDate(tempYear, monthDay)
+            else:
                 pollingDates['EndDate'].append(newDate)
-                endDateLastYear = True
-            if startDateLastYear & endDateLastYear:
+                
+            if startDateLastYear and endDateLastYear:
                 currYear = str(int(currYear) - 1)
-            
-        
-        '''
-        ##We went back a year
-        if (int(dateList[0][0]) == 12 & int(dateList[1][0] == 12)):
-            currYear = str(int(currYear) - 1)
-            
-        if (int(dateList[0][0]) <= 12 & int(dateList[1][0]) == 1):
-            currYear = '2017'
-        elif (int(dateList[0][0]) <= 12 & int(dateList[1][0]) <= 12):
-            pass
-        '''
+                yearModified = True
+                startDateLastYear = False
+                endDateLastYear = False
+        ##We are no longer in december, we can go back another year next time
+        ##we reach december
+        startMonth = dateList[0].split('/')
+        endMonth   = dateList[1].split('/')
+        if (int(startMonth[0]) == 11 and int(endMonth[0]) == 11):
+            yearModified = False
+    return pollingDates
         
         
+
+if __name__ == "__main__":
+    content = urllib2.urlopen("https://www.realclearpolitics.com/epolls/other/president_trump_job_approval-6179.html")
+    soup = BeautifulSoup(content,'html.parser')
+    tables = soup.findAll('table', {'class': 'data'})
+    
+    ##1st table in page index 0 is only recent polls, 2nd table index 1 is all data.
+    table = tables[1]
+    
+    rows = [row for row in table.find_all("tr")]
+    columns = [str(col.get_text()) for col in rows[0].find_all("th")]
+    pollingHeaders = [column.split('(')[0].strip() for column in columns[:]]
+    
+    pollingData = []
+    for row in rows:
+        tds = row.find_all("td")    
+        pollingData.append([(str(t.get_text())) for t in tds[:]])
+    
+    pollingDF = pd.DataFrame(data = pollingData[1:], columns = pollingHeaders)
+    
+    latestYear = '2018'
+    pollingDates = ModifyDatesForYear(pollingDF['Date'], latestYear)
+    
+    for keys in pollingDates.keys():
+        pollingDF[keys] = pollingDates[keys]
+    
