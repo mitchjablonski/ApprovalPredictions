1609517174072eb7632316d9e878ac661d94de86
bigdogstudio <mitchjabdota@gmail.com>
2018-04-12 21:50:00 -0400

    Makes a prediction of whether the trend will be positive or negative, and makes a plot to compare it

--- ParseRCPForData.py
+++ ParseRCPForData.py
@@ -18,6 +18,7 @@ from string import punctuation
 from wordcloud import WordCloud, STOPWORDS
 import sys
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 
 config = {}
 execfile("C:\Users\mitch\Desktop\Masters\DataMiningI\DataMiningProject\config.py", config)
@@ -176,6 +177,9 @@ def stop_words_for_wordcloud(stop_words):
     for word in stop_words:
         STOPWORDS.add(word)
 
+def get_words_from_dataframe(data, key):
+    return(clean_punctuation(data[key].add(' ').sum(axis = 0)).lower())
+
 def build_word_cloud(polling_df, out_dir, stop_words):
     stop_words_for_wordcloud(stop_words)
     snippet_words = ''
@@ -184,39 +188,47 @@ def build_word_cloud(polling_df, out_dir, stop_words):
     for row in range(rows):
         filepath = (out_dir + '\\' + my_query +  polling_df.iloc[row]['StartDate']+ '_' + polling_df.iloc[row]['EndDate'] +'.txt')
         data = pd.read_csv(filepath, delimiter="\t")
-        snippet_words += data['Snippet'].add(' ').sum(axis = 0)
-        title_words += data['Title'].add(' ').sum(axis = 0)
+        #snippet_words += data['Snippet'].add(' ').sum(axis = 0)
+        #title_words += data['Title'].add(' ').sum(axis = 0)
+        snippet_words += get_words_from_dataframe(data, 'Snippet')
+        title_words   +=get_words_from_dataframe(data, 'Title')
     
-    snippet_words = clean_punctuation(snippet_words)
-    title_words = clean_punctuation(title_words)    
+    #snippet_words = clean_punctuation(snippet_words)
+    #title_words = clean_punctuation(title_words)    
     save_word_cloud(snippet_words, 'snippet_word_cloud')
     save_word_cloud(title_words, 'title_word_cloud')
+'''
+def load_my_csv(filepath, delimiter):
+    return pd.read_csv(filepath, delimiter="\t")
+'''
+def determine_sentiment_from_words(pos_words, neg_words, words_to_check):
+    ##Less negative words count as positive for trump
+    overall_sentiment = 1
+    for word in words_to_check.split():
+        if word in pos_words:
+            overall_sentiment += 1
+        elif word in neg_words:
+            overall_sentiment -= 1
+    #print(overall_sentiment)
+    return overall_sentiment
 
-if __name__ == "__main__":
-    my_query = 'Donald Trump'
-    url = "https://www.realclearpolitics.com/epolls/other/president_trump_job_approval-6179.html"
-    #out_dir = "C:\Users\mitch\Desktop\Masters\DataMiningI\DataMiningProject\ApprovalPredictions\GoogleRequests"
-    out_dir = "C:\Users\mitch\Desktop\Masters\DataMiningI\DataMiningProject\ApprovalPredictions\TestExtraRequests"
-    polling_data_framename = 'polling_dataframe.txt'
-    #num_requests = 10
-    num_requests = 10
-    collect_new_data = False
-    populate_wordcloud = True
-    
-    stop_words = ['Donald', 'Trump', 'President', 'Washington', 
-                  'President', 'York', 'Jr', 'AP', 'White', 'House',
-                  'Monday', 'Tuesday', 'Wednesday', 
-                  'Thursday', 'Friday', 'Saturday', 'Sunday',
-                  'Jan', 'Feb', 'Mar', 'Apr', 'May', 
-                  'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 
-                  'Nov', 'Dec', 'say', 'said', 'talk', 
-                  'latest', 'sign', 'first', 'wants', 
-                  'day', 'tell', 'second', 'Associated', 
-                  'Press', 'new', 'will', 'call', 
-                  'times', 'local', 'one', 'says', 'calls']
-    ##1st table in page index 0 is only recent polls, 2nd table index 1 is all data.
-    table_index = 1
-    latest_year = '2018'
+def predict_approval_rating_change(polling_data, out_dir, pos_words, neg_words):
+    rows, _ = polling_data.shape
+    sentiment_values = defaultdict(list)
+    for row in range(rows):
+        filepath = (out_dir + '\\' + my_query +  polling_data.iloc[row]['StartDate']+ '_' + polling_data.iloc[row]['EndDate'] +'.txt')
+        data = pd.read_csv(filepath, delimiter="\t")
+        snippet_words = get_words_from_dataframe(data, 'Snippet')
+        title_words   = get_words_from_dataframe(data, 'Title')
+        #polling_data.iloc[row]['SnippetPred'] = determine_sentiment_from_words(pos_words, neg_words, snippet_words)
+        sentiment_values['snippet_words'].append(determine_sentiment_from_words(pos_words, neg_words, snippet_words))
+        #polling_data.iloc[row]['TitlePred'] = determine_sentiment_from_words(pos_words, neg_words, title_words)
+        sentiment_values['title_words'].append(determine_sentiment_from_words(pos_words, neg_words, title_words))
+    return sentiment_values
+
+def Main(my_query, url, out_dir, polling_data_framename, 
+         num_requests, collect_new_data, populate_wordcloud, 
+         positive_words, negative_words, stop_words, table_index, latest_year):
     
     if collect_new_data:
         polling_df = parse_html_for_table(url, table_index)
@@ -237,5 +249,82 @@ if __name__ == "__main__":
     
     if populate_wordcloud:
         build_word_cloud(reuters_df, out_dir, stop_words)
+    
+    reuters_df['ApprovalChange'] = reuters_df.loc[:,'Approve'].diff().fillna(0)
+    reuters_df['ApprovalPosChange'] = (reuters_df.loc[:,'ApprovalChange'] >= 0)
+    #reuters_df['SnippetPred'] = 0
+    #reuters_df['TitlePred'] = 0
+    #temp_df = 
+    sentiment_values = predict_approval_rating_change(reuters_df, out_dir, positive_words, negative_words)
+    reuters_df['SnippetChange'] = sentiment_values['snippet_words']
+    reuters_df['SnippetPosPred'] = (reuters_df.loc[:,'SnippetChange'] >= 0)
+    reuters_df['TitleChange'] = sentiment_values['title_words']
+    reuters_df['TitlePosPred'] = (reuters_df.loc[:,'TitleChange'] >= 0)
+    determine_prediction_accuracy_and_plot(reuters_df, out_dir)
+    return reuters_df #predict_approval_rating_change(reuters_df, out_dir, positive_words, negative_words)
+
+def determine_prediction_accuracy_and_plot(polling_results_df, out_dir):
+    polling_results_df['SnippetAccuracy'] = (polling_results_df.loc[:,'SnippetPosPred'] == polling_results_df.loc[:,'ApprovalPosChange'])
+    polling_results_df['TitleAccuracy'] = (polling_results_df.loc[:,'TitlePosPred'] == polling_results_df.loc[:,'ApprovalPosChange'])
+    plot_accuracy_data(polling_results_df, out_dir)
+    
+def plot_accuracy_data(polling_results_df, out_dir):
+    x = [datetime.strptime(d,'%Y-%m-%d').date() for d in polling_results_df['StartDate']]
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d/%Y'))
+    #plt.gca().xaxis.set_major_locator(mdates.DayLocator())
+    plt.plot(x,modified_polling_df['TitleAccuracy'])
+    plt.gcf().autofmt_xdate()
+    plt.title('Using Article Title Prediction Accuracy')
+    plt.savefig(out_dir + '//titleaccuracyplot.png', dpi=300)
+    plt.plot(x,modified_polling_df['SnippetAccuracy'])
+    plt.title('Comparing Title and Snippet Prediction Accuracy')
+    plt.savefig(out_dir + '//snippet_and_titleaccuracyplot.png', dpi=300)
+    plt.clf()
+    plt.title('Using Snippet Prediction Accuracy')
+    plt.plot(x,modified_polling_df['SnippetAccuracy'])
+    plt.savefig(out_dir + '//snippetaccuracyplot.png', dpi=300)
+    
+if __name__ == "__main__":
+    my_query = 'Donald Trump'
+    url = "https://www.realclearpolitics.com/epolls/other/president_trump_job_approval-6179.html"
+    #out_dir = "C:\Users\mitch\Desktop\Masters\DataMiningI\DataMiningProject\ApprovalPredictions\GoogleRequests"
+    out_dir = "C:\Users\mitch\Desktop\Masters\DataMiningI\DataMiningProject\ApprovalPredictions\TestExtraRequests"
+    polling_data_framename = 'polling_dataframe.txt'
+    #num_requests = 10
+    num_requests = 10
+    collect_new_data = False
+    populate_wordcloud = False
+    positive_words = ['deal', 'iran', 'rally', 
+                      'aid', 'won', 'top', 'mexico', 
+                      'taxes', 'tax', 'plan', 'korea', 'military',
+                      'leader', 'presidential', 'clinton', 'china', 
+                      'trade', 'health','war', 'international', 'global', 'credit']
+    negative_words = ['investigation', 'porn', 'russia', 
+                      'russian', 'fbi', 'counsel', 'special', 'russian',
+                      'putin', 'election', 'attorney', 'mueller',
+                      'twitter', 'palm', 'probe', 'flordia',
+                      'comey', 'fact', 'lawyer', 'stormy', 'flynn', 'tweet']
+    
+    stop_words = ['Donald', 'Trump', 'President', 'Washington', 
+                  'President', 'York', 'Jr', 'AP', 'White', 'House',
+                  'Monday', 'Tuesday', 'Wednesday', 
+                  'Thursday', 'Friday', 'Saturday', 'Sunday',
+                  'Jan', 'Feb', 'Mar', 'Apr', 'May', 
+                  'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 
+                  'Nov', 'Dec', 'say', 'said', 'talk', 
+                  'latest', 'sign', 'first', 'wants', 
+                  'day', 'tell', 'second', 'Associated', 
+                  'Press', 'new', 'will', 'call', 
+                  'times', 'local', 'one', 'says', 'calls']
+    ##1st table in page index 0 is only recent polls, 2nd table index 1 is all data.
+    table_index = 1
+    latest_year = '2018'
+    
+    modified_polling_df = Main(my_query, url, out_dir, polling_data_framename, 
+                               num_requests, collect_new_data, populate_wordcloud, 
+                               positive_words, negative_words, stop_words, table_index, latest_year)
+
+              
+    
     ## to read back in the data we write: data = pd.read_csv(filepath, delimiter="\t")
     
