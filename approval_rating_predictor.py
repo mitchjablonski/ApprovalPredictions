65896f6f75e704c7d7142856c8fbfde5ef6d8cc1
bigdogstudio <mitchjabdota@gmail.com>
2018-05-07 20:36:38 -0400

    Lots of changes that need cleenup.
    Need to cleanup

--- approval_rating_predictor.py
+++ approval_rating_predictor.py
@@ -12,6 +12,7 @@ from apiclient.discovery import build
 #import urllib2
 #from bs4 import BeautifulSoup
 import pandas as pd
+import numpy as np
 from collections import defaultdict, Counter
 from datetime import datetime, timedelta
 from string import punctuation
@@ -21,6 +22,13 @@ import post_process_and_plot as post_process
 import parse_and_clean_html_table as parse_and_clean
 from sklearn.model_selection import train_test_split
 from nltk.stem.snowball import SnowballStemmer
+from nltk.corpus import stopwords
+from sklearn.metrics import accuracy_score
+from sklearn.feature_extraction.text import CountVectorizer
+from nltk.classify import NaiveBayesClassifier
+import nltk.classify.util
+from functools import partial
+from multiprocessing import Pool
 
 
 config = {}
@@ -122,8 +130,9 @@ def build_word_cloud(polling_df, out_dir, stop_words):
 
 def determine_sentiment_from_words(pos_words, neg_words, words_to_check):
     ##Less negative words count as positive for trump
-    overall_sentiment = 1
-    for word in words_to_check.split():
+    #overall_sentiment = 1
+    overall_sentiment = 0
+    for word in words_to_check:
         if word in pos_words:
             overall_sentiment += 1
         elif word in neg_words:
@@ -137,10 +146,11 @@ def stem_words(words_to_stem):
         words_ret.append(stemmer.stem(words))
     return words_ret
 
-def determine_sentiment_auto(polling_df, out_dir):
-    X_train, X_test, y_train, y_test = train_test_split(polling_df[['StartDate','EndDate']], polling_df['ApprovalPosChange'])
+def gather_words_train_set(polling_df, out_dir, junk_words, X_train):
     positive_counter = Counter()
     negative_counter = Counter()
+    all_pos_words = []
+    all_neg_words = []
     for index, row in X_train.iterrows():
         filepath = (out_dir + '\\' + my_query +  row['StartDate']+ '_' + row['EndDate'] +'.txt')
         data = pd.read_csv(filepath, delimiter="\t")
@@ -150,12 +160,119 @@ def determine_sentiment_auto(polling_df, out_dir):
         all_words = stem_words(snippet_words.split() + title_words.split())
         if polling_df.loc[index, 'ApprovalPosChange'] == 1:
             for word in all_words:
-                positive_counter[word] += 1
+                all_pos_words.append(word)
+                if word not in junk_words:
+                    positive_counter[word] += 1
         else:
             for word in all_words:
-                ##If we don't add all -1 will be weighted equally
-                negative_counter[word] += 1
-    return (positive_counter, negative_counter)
+                all_neg_words.append(word)
+                ##If we don't add all -1 will be weighted equally in most_common
+                if word not in junk_words:
+                    negative_counter[word] += 1    
+    return positive_counter, negative_counter, all_pos_words, all_neg_words
+
+def determine_pos_neg_word_split(positive_counter, negative_counter, num_words_to_use):
+    positive_words = []
+    negative_words = []
+    tot_words_sentiment = []
+    all_words = [words for words,_ in positive_counter.most_common(num_words_to_use)]
+    all_words += [words for words, _ in negative_counter.most_common(num_words_to_use)]
+    for word in all_words:
+        if positive_counter[word] - negative_counter[word] > 0:
+            positive_words.append(word)
+            tot_words_sentiment.append((word, 'pos'))
+        else:
+            negative_words.append(word)
+            tot_words_sentiment.append((word,'neg'))
+    return positive_words, negative_words, tot_words_sentiment
+
+def perform_analysis_test_set(polling_df, out_dir, pos_words, neg_words, X_test):
+    sentiment_values = defaultdict(list)
+    for index, row in X_test.iterrows():
+        filepath = (out_dir + '\\' + my_query +  row['StartDate']+ '_' + row['EndDate'] +'.txt')
+        data = pd.read_csv(filepath, delimiter="\t")
+        snippet_words = stem_words(get_words_from_dataframe(data, 'Snippet').split())
+        title_words   = stem_words(get_words_from_dataframe(data, 'Title').split())
+        sentiment_values['snippet_words_auto'].append(determine_sentiment_from_words(pos_words, neg_words, snippet_words)) 
+        sentiment_values['title_words_auto'].append(determine_sentiment_from_words(pos_words, neg_words, title_words)) 
+    return sentiment_values
+
+def get_word_features(all_words):
+    wordlist = nltk.FreqDist(all_words)
+    features = wordlist.keys()
+    return features
+
+def extract_features(w_features, wordset):
+    docwords = set(wordset)
+    features = {}
+    for word in w_features:
+        features['constains'.format(word)] = (word in words)
+
+def determine_sentiment_auto(polling_df, out_dir, junk_words, test_train_split_word_count, num_iters_auto):
+    accuracy_list = []
+    for iters in range(num_iters_auto):
+        X_train, X_test, y_train, y_test = train_test_split(polling_df[['StartDate','EndDate']], polling_df['ApprovalPosChange'])
+        positive_counter, negative_counter, all_pos_words, all_neg_words = gather_words_train_set(polling_df, out_dir, junk_words, X_train)
+        pos_words, neg_words, tot_words_sentiment = determine_pos_neg_word_split(positive_counter, negative_counter, test_train_split_word_count)
+        '''
+        Attempt to use classifier...
+        '''
+        #all_words = all_pos_words + all_neg_words
+        #w_features = get_word_features(all_words)
+        #partial_extract = partial(extract_features, w_features)
+        #training_set = nltk.classify.apply_features(partial_extract, tot_words_sentiment)
+        #classifier = nltk.NaiveBayesClassifier.train(training_set)
+        sentiment_values = perform_analysis_test_set(polling_df, out_dir, pos_words, neg_words, X_test)
+        temp_df = pd.DataFrame()
+        temp_df['SnippetChangeAuto'] = sentiment_values['snippet_words_auto']
+        temp_df['SnippetPosPredAuto'] = (temp_df.loc[:,'SnippetChangeAuto'] >= 0)
+        temp_df['TitleChangeAuto'] = sentiment_values['title_words_auto']
+        temp_df['TitlePosPredAuto'] = (temp_df.loc[:,'TitleChangeAuto'] >= 0)
+        print(accuracy_score(y_test, temp_df['SnippetPosPredAuto']))
+        acc_score = accuracy_score(y_test, temp_df['SnippetPosPredAuto'])
+        accuracy_list.append(acc_score)
+
+    mean_acc = reduce(lambda x,y: x+y, accuracy_list) / len(accuracy_list)
+    print('Mean Accuracy {}'.format(mean_acc))
+
+    #count_vect = CountVectorizer(count_vect_filelist)
+    #print(count_vect)
+    #X_train_counts = count_vect.fit_transform(X_train)
+    '''
+    pos_dict = dict([(word, True) for word in all_pos_words])
+    neg_dict = dict([(word, True) for word in all_neg_words])
+    pos_feats = (pos_dict, 'pos')
+    neg_feats = (neg_dict, 'neg')
+    #pos_feats = [(str(words), 'pos') for words in all_pos_words]
+    #neg_feats = [(str(words), 'neg') for words in all_neg_words]
+
+    negcutoff = len(neg_feats)*3//4
+    poscutoff = len(pos_feats)*3//4
+    train_feats = neg_feats[:negcutoff] + pos_feats[:poscutoff]
+    test_feats = neg_feats[negcutoff:] + pos_feats[poscutoff:]
+    classifier = NaiveBayesClassifier.train(train_feats)
+    print 'accuracy:', nltk.classify.util.accuracy(classifier, test_feats)
+    classifier.show_most_important_features()
+    #print(pos_feats)
+    '''
+    return (positive_counter, negative_counter, mean_acc)
+
+def determine_sentiment_full_set(polling_df, out_dir, junk_words, full_set_word_count):
+    ##We should be able to provide the polling df as the test set and have this work...
+    positive_counter, negative_counter, all_pos_words, all_neg_words = gather_words_train_set(polling_df, out_dir, junk_words, polling_df)
+    pos_words, neg_words,_ = determine_pos_neg_word_split(positive_counter, negative_counter, full_set_word_count)
+    sentiment_values = perform_analysis_test_set(polling_df, out_dir, pos_words, neg_words, polling_df)
+    polling_df['SnippetChangeAuto'] = sentiment_values['snippet_words_auto']
+    polling_df['SnippetPosPredAuto'] = (polling_df.loc[:,'SnippetChangeAuto'] >= 0)
+    polling_df['TitleChangeAuto'] = sentiment_values['title_words_auto']
+    polling_df['TitlePosPredAuto'] = (polling_df.loc[:,'TitleChangeAuto'] >= 0)
+    '''
+    msk = np.random.rand(len(polling_df)) < 0.8
+    train = polling_df.loc[msk]
+    test = polling_df.loc[~msk]
+    '''
+
+    return polling_df
 
 def predict_approval_rating_change(polling_data, out_dir, pos_words, neg_words):
     rows, _ = polling_data.shape
@@ -163,8 +280,8 @@ def predict_approval_rating_change(polling_data, out_dir, pos_words, neg_words):
     for row in range(rows):
         filepath = (out_dir + '\\' + my_query +  polling_data.iloc[row]['StartDate']+ '_' + polling_data.iloc[row]['EndDate'] +'.txt')
         data = pd.read_csv(filepath, delimiter="\t")
-        snippet_words = get_words_from_dataframe(data, 'Snippet')
-        title_words   = get_words_from_dataframe(data, 'Title')
+        snippet_words = get_words_from_dataframe(data, 'Snippet').split()
+        title_words   = get_words_from_dataframe(data, 'Title').split()
         sentiment_values['snippet_words'].append(determine_sentiment_from_words(pos_words, neg_words, snippet_words))
         sentiment_values['title_words'].append(determine_sentiment_from_words(pos_words, neg_words, title_words))
     return sentiment_values
@@ -183,7 +300,9 @@ def personal_sentiment_analysis(reuters_df, out_dir, positive_words, negative_wo
 
 def _main(my_query, url, out_dir, polling_data_framename, 
          num_requests, collect_new_data, populate_wordcloud, 
-         positive_words, negative_words, stop_words, table_index, latest_year):
+         positive_words, negative_words, stop_words, table_index, 
+         latest_year, junk_words, num_iters_auto, test_train_split_word_count,
+         full_set_word_count):
     
     if collect_new_data:
         polling_df = parse_and_clean.parse_html_for_table(url, table_index)
@@ -199,18 +318,26 @@ def _main(my_query, url, out_dir, polling_data_framename,
     
     #pollsters = set(polling_df['Poll'])
     ##Reuters is most common Pollster
-    reuters_df = polling_df[polling_df['Poll'] == 'Reuters/IpsosReuters'].copy()
+    reuters_df = polling_df.loc[polling_df['Poll'] == 'Reuters/IpsosReuters'].copy()
     if collect_new_data:
         request_and_write_google_results(reuters_df, out_dir, num_requests)
     
     if populate_wordcloud:
         build_word_cloud(reuters_df, out_dir, stop_words)
+
     reuters_df = personal_sentiment_analysis(reuters_df, out_dir, positive_words, negative_words)
 
-    pos_count, neg_count = determine_sentiment_auto(reuters_df, out_dir)
-    print(pos_count.most_common(20),neg_count.most_common(20))
+    pos_count, neg_count, acc_score = determine_sentiment_auto(reuters_df, out_dir, junk_words, test_train_split_word_count, num_iters_auto)
+    #iter_list = [i for i in range(num_iters_auto)]
+    #partial_auto = partial(determine_sentiment_auto, reuters_df, out_dir, junk_words, test_train_split_word_count)
+    #pool = Pool()
+    #accuracy_list = pool.map(partial_auto, iter_list)
+    #acc_score = reduce(lambda x,y: x+y, accuracy_list) / len(accuracy_list)
+    #print('Mean Accuracy {}'.format(acc_score))
+
+    reuters_df = determine_sentiment_full_set(reuters_df, out_dir, junk_words, full_set_word_count)
     #nltk_sentiment_analysis(reuters_df, out_dir)
-    post_process.run_post_processing(reuters_df, out_dir)
+    post_process.run_post_processing(reuters_df, out_dir, acc_score)
 
     return reuters_df 
             
@@ -220,7 +347,7 @@ if __name__ == "__main__":
     #out_dir = "C:\Users\mitch\Desktop\Masters\DataMiningI\DataMiningProject\ApprovalPredictions\GoogleRequests"
     out_dir = "C:\Users\mitch\Desktop\Masters\DataMiningI\DataMiningProject\ApprovalPredictions\TestExtraRequests"
     polling_data_framename = 'polling_dataframe.txt'
-    #num_requests = 10
+
     num_requests = 10
     collect_new_data = False
     populate_wordcloud = False
@@ -229,6 +356,7 @@ if __name__ == "__main__":
                       'taxes', 'tax', 'plan', 'korea', 'military',
                       'leader', 'presidential', 'clinton', 'china', 
                       'trade', 'health','war', 'international', 'global', 'credit']
+
     negative_words = ['investigation', 'porn', 'russia', 
                       'russian', 'fbi', 'counsel', 'special', 'russian',
                       'putin', 'election', 'attorney', 'mueller',
@@ -246,15 +374,37 @@ if __name__ == "__main__":
                   'day', 'tell', 'second', 'Associated', 
                   'Press', 'new', 'will', 'call', 
                   'times', 'local', 'one', 'says', 'calls']
+
+    #junk_words = set(stopwords.words('english'))
+    #Words should come from NLTK stopwords
+    junk_words = []
+    
+    junk_words = ['ourselves', 'hers', 'between', 'yourself', 'but', 'again', 'there', 
+                  'about', 'once', 'during', 'out', 'very', 'having', 'with', 'they', 'own', 'an', 'be', 'some', 
+                  'for', 'do', 'its', 'yours', 'such', 'into', 'of', 'most', 'itself', 'other', 'off', 'is', 's', 
+                  'am', 'or', 'who', 'as', 'from', 'him', 'each', 'the', 'themselves', 'until', 'below', 'are', 'we', 
+                  'these', 'your', 'his', 'through', 'don', 'nor', 'me', 'were', 'her', 'more', 'himself', 'this',
+                   'down', 'should', 'our', 'their', 'while', 'above', 'both', 'up', 'to', 'ours', 'had',
+                    'she', 'all', 'no', 'when', 'at', 'any', 'before', 'them', 'same', 'and', 'been', 'have', 
+                    'in', 'will', 'on', 'does', 'yourselves', 'then', 'that', 'because', 'what', 'over', 'why',
+                     'so', 'can', 'did', 'not', 'now', 'under', 'he', 'you', 'herself', 'has', 'just', 'where', 
+                     'too', 'only', 'myself', 'which', 'those', 'i', 'after', 'few', 'whom', 't', 'being', 'if', 
+                     'theirs', 'my', 'against', 'a', 'by', 'doing', 'it', 'how', 'further', 'was', 'here', 'than']
+    
+    junk_words += [words.lower() for words in stop_words]
+    stemmer = SnowballStemmer("english")
+    junk_words = [stemmer.stem(words) for words in junk_words]
+    addtl_years = ['2018', '2017']
+    junk_words += addtl_years
     ##1st table in page index 0 is only recent polls, 2nd table index 1 is all data.
     table_index = 1
     latest_year = '2018'
+    num_iters_auto = 100
+    test_train_split_word_count = 500
+    full_set_word_count = 25
     
     modified_polling_df = _main(my_query, url, out_dir, polling_data_framename, 
                                 num_requests, collect_new_data, populate_wordcloud, 
-                                positive_words, negative_words, stop_words, table_index, latest_year)
-
-              
-    
-    ## to read back in the data we write: data = pd.read_csv(filepath, delimiter="\t")
-    
+                                positive_words, negative_words, stop_words, table_index, 
+                                latest_year, junk_words, num_iters_auto, test_train_split_word_count,
+                                full_set_word_count)
--- post_process_and_plot.py
+++ post_process_and_plot.py
@@ -9,54 +9,129 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 from datetime import datetime
+import numpy as np
+import seaborn as sns
 
 def determine_prediction_accuracy_and_plot(polling_results_df, out_dir):
     polling_results_df['SnippetAccuracy'] = (polling_results_df.loc[:,'SnippetPosPred'] == polling_results_df.loc[:,'ApprovalPosChange'])
     polling_results_df['TitleAccuracy'] = (polling_results_df.loc[:,'TitlePosPred'] == polling_results_df.loc[:,'ApprovalPosChange'])
-    plot_accuracy_data(polling_results_df, out_dir)
+    polling_results_df['SnippetAccuracyAuto'] = (polling_results_df.loc[:,'SnippetPosPredAuto'] == polling_results_df.loc[:,'ApprovalPosChange'])
+    polling_results_df['TitleAccuracyAuto'] = (polling_results_df.loc[:,'TitlePosPredAuto'] == polling_results_df.loc[:,'ApprovalPosChange'])
+    snippet_col_acc = 'SnippetAccuracy'
+    snippet_col_pred = 'SnippetPosPred'
+    title_col_acc = 'TitleAccuracy'
+    title_col_pred = 'TitlePosPred'
+    pred_type = 'Manual'
+    plot_accuracy_data(polling_results_df, out_dir, snippet_col_acc, snippet_col_pred, title_col_acc, title_col_pred, pred_type)
+    snippet_col_acc = 'SnippetAccuracyAuto'
+    snippet_col_pred = 'SnippetPosPredAuto'
+    title_col_acc = 'TitleAccuracyAuto'
+    title_col_pred = 'TitlePosPredAuto'
+    pred_type = 'Auto'
+    plot_accuracy_data(polling_results_df, out_dir, snippet_col_acc, snippet_col_pred, title_col_acc, title_col_pred, pred_type)
     
-def plot_accuracy_data(polling_results_df, out_dir):
+def plot_accuracy_data(polling_results_df, out_dir, snippet_col_acc, snippet_col_pred, title_col_acc, title_col_pred, pred_type):
+
     x = [datetime.strptime(d,'%Y-%m-%d').date() for d in polling_results_df['StartDate']]
     plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d/%Y'))
     #plt.gca().xaxis.set_major_locator(mdates.DayLocator())
-    plt.plot(x,polling_results_df['TitleAccuracy'], label = 'Title Change')
+    plt.plot(x,polling_results_df[title_col_acc], label = 'Title Change')
     plt.gcf().autofmt_xdate()
-    plt.title('Using Article Title Predicted Change')
+    plt.title('Using Article Title Predicted Change {}'.format(pred_type))
     plt.plot(x,polling_results_df['ApprovalPosChange'], label = 'Actual Change')
     leg = plt.legend(loc = 2)
     leg.get_frame().set_alpha(0.1)
-    plt.savefig(out_dir + '//titleaccuracyplot.png', dpi=300)
+    plt.savefig(out_dir + '//titleaccuracyplot{}.png'.format(pred_type.lower()), dpi=300)
     
     plt.clf()
-    plt.plot(x,polling_results_df['TitleAccuracy'], label = 'Title Change')
+    plt.plot(x,polling_results_df[title_col_acc], label = 'Title Change')
     plt.gcf().autofmt_xdate()
-    plt.plot(x,polling_results_df['SnippetAccuracy'], label = 'Snippet Change')
-    plt.title('Comparing Title and Snippet Predicted Changes')
+    plt.plot(x,polling_results_df[snippet_col_acc], label = 'Snippet Change')
+    plt.title('Comparing Title and Snippet Predicted Changes {}'.format(pred_type.lower()))
     leg = plt.legend(loc = 2)
     leg.get_frame().set_alpha(0.1)
-    plt.savefig(out_dir + '//snippet_and_titleaccuracyplot.png', dpi=300)
+    plt.savefig(out_dir + '//snippet_and_titleaccuracyplot{}.png'.format(pred_type), dpi=300)
     
     plt.clf()
     plt.title('Using Snippet Predicted Change')
-    plt.plot(x,polling_results_df['SnippetAccuracy'])
+    plt.plot(x,polling_results_df[snippet_col_acc])
     plt.plot(x,polling_results_df['ApprovalPosChange'], label = 'Actual Change')
     leg = plt.legend(loc = 2)
     leg.get_frame().set_alpha(0.1)
-    plt.savefig(out_dir + '//snippetaccuracyplot.png', dpi=300)
+    plt.savefig(out_dir + '//snippetaccuracyplot{}.png'.format(pred_type.lower()), dpi=300)
+
+    plot_cols = [[snippet_col_acc, snippet_col_pred, 'Snippet'], [title_col_acc, title_col_pred, 'Title']]
+
+    actual_pos_weeks,_ = polling_results_df.loc[polling_results_df['ApprovalPosChange'] == True].shape
+    total_weeks, _ = polling_results_df.shape
+    actual_neg_weeks = total_weeks - actual_pos_weeks
+
+    for cols in plot_cols:
+        plt.clf()
+        fig, ax = plt.subplots()
+        bar_width = 0.35
+        opacity = 0.8
+        n_groups = 3
+        index = np.arange(n_groups)
+
+        correct_weeks,_ = polling_results_df.loc[polling_results_df[cols[0]] == True].shape
+        pred_pos_weeks,_ = polling_results_df.loc[polling_results_df[cols[1]] == True].shape
+        pred_neg_weeks = total_weeks - pred_pos_weeks
+
+        pred_data = (correct_weeks, pred_pos_weeks, pred_neg_weeks)
+        actual_data = (total_weeks, actual_pos_weeks, actual_neg_weeks)
+
+        rects1 = plt.bar(index + bar_width, actual_data, bar_width,
+                         alpha=opacity,
+                         color='g',
+                         label='Actual Data')
+
+        rects2 = plt.bar(index, pred_data, bar_width,
+                        alpha=opacity,
+                        color='b',
+                        label='Predicted {} Data'.format(cols[2]))
+        #plt.xlabel('')
+        plt.ylabel('Weeks')
+        plt.title('{} Prediction {} vs Actuals'.format(pred_type, cols[2]))
+        plt.xticks(index + bar_width/2, ('Total Weeks Correct', 'Positive Weeks', 'Negative Weeks'))
+        plt.legend()
+
+        plt.tight_layout()
+        plt.savefig(out_dir + '//bargraph{}{}.png'.format(pred_type, cols[2]))
+        plt.clf()
+
+
     
-def create_data_report(data,out_dir):
+def create_data_report(data, out_dir, acc_score, snippet_col, title_col, pred_type):
     total_rows,_ = data.shape
-    snippet_correct_rows,_ = data[data['SnippetPosPred'] == True].shape
-    title_correct_rows,_ = data[data['TitlePosPred'] == True].shape
-    new_file = out_dir + '\\' +'output_data_overall_report.txt'
+    pos_weeks,_ = data.loc[data['ApprovalPosChange'] == True].shape
+    neg_weeks = total_rows - pos_weeks
+    snippet_correct_rows,_ = data.loc[data[snippet_col] == True].shape
+    title_correct_rows,_ = data.loc[data[title_col] == True].shape
+    new_file = out_dir + '\\' +'output_data_overall_report{}.txt'.format(pred_type)
     with open(new_file, 'w') as curr_file:
-            curr_file.write('Total_Data_Eval\tTitle_Data_Raw_Correct\tTitle_Data_Accuracy\tSnippet_Data_Raw_Correct\tSnippet_Data_Accuracy\n')
+            curr_file.write('Total_Data_Eval\tTotal_Pos_Weeks\tTotal_Neg_weeks\tTitle_Data_Raw_Correct\tTitle_Data_Accuracy\tSnippet_Data_Raw_Correct\tSnippet_Data_Accuracy\tTestTrainSplitAccuracyScore\n')
             title_accuracy = (title_correct_rows/total_rows)*100
             snippet_accuracy = (snippet_correct_rows/total_rows)*100
-            curr_file.write(str(total_rows) +'\t' + str(title_correct_rows) + '\t' + str(title_accuracy)
-                            + '\t' + str(snippet_correct_rows) + '\t' + str(snippet_accuracy))
+            curr_file.write(str(total_rows) + '\t' + str(pos_weeks) +'\t' + str(neg_weeks)  +'\t' + str(title_correct_rows) + '\t' + str(title_accuracy)
+                            + '\t' + str(snippet_correct_rows) + '\t' + str(snippet_accuracy) + '\t' + str(acc_score))
 
 
-def run_post_processing(polling_results_df, out_dir):
+def run_post_processing(polling_results_df, out_dir, acc_score):
     determine_prediction_accuracy_and_plot(polling_results_df, out_dir)
-    create_data_report(polling_results_df, out_dir)
+    '''
+    snippet_col = 'SnippetPosPred'
+    title_col = 'TitlePosPred'
+    '''
+    snippet_col = 'SnippetAccuracy'
+    title_col = 'TitleAccuracy'
+    pred_type = '_manual'
+    create_data_report(polling_results_df, out_dir, acc_score, snippet_col, title_col, pred_type)
+    '''
+    snippet_col = 'SnippetPosPredAuto'
+    title_col = 'TitlePosPredAuto'
+    '''
+    snippet_col = 'SnippetAccuracyAuto'
+    title_col = 'TitleAccuracyAuto'
+    pred_type = '_auto'
+    create_data_report(polling_results_df, out_dir, acc_score, snippet_col, title_col, pred_type)
